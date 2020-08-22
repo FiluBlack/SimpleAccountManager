445c56a2727654249551f790c3e16e8650959369
FiluBlack <38252422+FiluBlack@users.noreply.github.com>
2020-08-22 20:01:59 +0200

    Updated Singup

--- SimpleAccountManager/Signup.cpp
+++ SimpleAccountManager/Signup.cpp
@@ -1,1 +1,57 @@
 #include "Signup.h"
+#include<iostream>
+
+Signup::Signup()
+{
+	Email email = inputEmail();
+	Username username = inputUsername();
+	Password password = inputPassword();
+	newAccount = Account(email, username, password);
+}
+
+Signup::~Signup()
+{
+}
+
+Account Signup::getAccount()
+{
+	return newAccount;
+}
+
+Email Signup::inputEmail()
+{
+	char email;
+	while (true)
+	{
+		std::cout << "Email: ";
+		std::cin >> email;
+		if (Email.exist(email)) std::cout << "Email already exist!" << std::endl;
+		else if (!Email.valid(email)) std::cout << "Email is not valid!" << std::endl;
+		else return Email(email);
+	}
+}
+
+Username Signup::inputUsername()
+{
+	char username;
+	while (true)
+	{
+		std::cout << "Username: ";
+		std::cin >> username;
+		if (Username.exist(username)) std::cout << "Username already exist!" << std::endl;
+		else if (!Username.valid(username)) std::cout << "Username is not valid!" << std::endl;
+		else return Username(username);
+	}
+}
+
+Password Signup::inputPassword()
+{
+	char password;
+	while (true)
+	{
+		std::cout << "Password: ";
+		std::cin >> password;
+		if (!Password.valid(password)) std::cout << "Password is not valid!" << std::endl;
+		else return Password(password);
+	}
+}
--- SimpleAccountManager/Signup.h
+++ SimpleAccountManager/Signup.h
@@ -1,5 +1,22 @@
 #pragma once
+#include "Account.h"
+#include "Email.h"
+#include "Username.h"
+#include "Password.h"
+
 class Signup
 {
+private:
+	Account newAccount;
+
+public:
+	Signup();
+	~Signup();
+
+	Account getAccount();
+
+	Email inputEmail();
+	Username inputUsername();
+	Password inputPassword();
 };
 
