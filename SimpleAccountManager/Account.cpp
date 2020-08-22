a5ebaaf9bef376ac946138a73c1a307aa1319800
FiluBlack <38252422+FiluBlack@users.noreply.github.com>
2020-08-22 19:30:19 +0200

    Updated Account Class

--- SimpleAccountManager/Account.cpp
+++ SimpleAccountManager/Account.cpp
@@ -1,1 +1,29 @@
 #include "Account.h"
+
+Account::Account(Email email, Username username, Password password)
+{
+}
+
+Account::~Account()
+{
+}
+
+Account Account::findAccountByID()
+{
+    return Account();
+}
+
+Account Account::findAccountByUsername()
+{
+    return Account();
+}
+
+AccountList Account::giveAllByID()
+{
+    return AccountList();
+}
+
+AccountList Account::giveAllByUsername()
+{
+    return AccountList();
+}
--- SimpleAccountManager/Account.h
+++ SimpleAccountManager/Account.h
@@ -1,5 +1,28 @@
 #pragma once
+
+#include "AccountList.h"
+#include "UserID.h"
+#include "Username.h"
+#include "Email.h"
+#include "Password.h"
 class Account
 {
+private:
+	char name;
+	UserID accountID;
+	Email accountEmail;
+	Password accountPassword;
+
+	enum AccountOptions {SEARCHACCOUNT=1};
+public:
+	Username accountUsername;
+
+	Account(Email email, Username username, Password password);
+	~Account();
+
+	static Account findAccountByID();
+	static Account findAccountByUsername();
+	static AccountList giveAllByID();
+	static AccountList giveAllByUsername();
 };
 
