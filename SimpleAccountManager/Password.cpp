7bff56ea716d206d9898483424f033c89ac7ea44
FiluBlack <38252422+FiluBlack@users.noreply.github.com>
2020-08-23 15:09:21 +0200

    Updated the Password class

--- SimpleAccountManager/Password.cpp
+++ SimpleAccountManager/Password.cpp
@@ -1,1 +1,44 @@
 #include "Password.h"
+
+char Password::hashPassword(char password)
+{
+	char hashedPassword = password;
+	return hashedPassword;
+}
+
+Password::Password(char passwordChar)
+{
+	if (isValid(passwordChar))
+	{
+	 hashedPassword = hashPassword(passwordChar);
+	}
+	else
+	{
+		std::cout << "Password is not valid!" << std::endl;
+		this->~Password();
+	}
+}
+
+Password::~Password()
+{
+}
+
+bool Password::isValid(char password)
+{
+	return true;
+}
+
+bool Password::isCorrectPassword(char password)
+{
+	if (hashedPassword == hashPassword(password)) return true;
+	return false;
+}
+
+Password& Password::operator=(const char& assignedPasword)
+{
+	if (isValid(assignedPasword))
+	{
+		hashedPassword = hashPassword(assignedPasword);
+		return *this;
+	}
+}
--- SimpleAccountManager/Password.h
+++ SimpleAccountManager/Password.h
@@ -1,5 +1,19 @@
 #pragma once
+#include<iostream>
+
 class Password
 {
+private:
+	char hashedPassword;
+
+	static char hashPassword(char password);
+public:
+	Password(char passwordChar);
+	~Password();
+
+	static bool isValid(char password);
+	bool isCorrectPassword(char password);
+
+	Password& operator=(const char &assignedPasword);
 };
 
