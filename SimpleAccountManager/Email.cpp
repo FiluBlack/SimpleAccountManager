d32df8db720b799df5feb092bd3438adab62a9a6
FiluBlack <38252422+FiluBlack@users.noreply.github.com>
2020-08-23 11:58:51 +0200

    Updated Email class

--- SimpleAccountManager/Email.cpp
+++ SimpleAccountManager/Email.cpp
@@ -1,1 +1,32 @@
 #include "Email.h"
+
+Email::Email(char emailChar)
+{
+	email = emailChar;
+}
+
+Email::~Email()
+{
+}
+
+bool Email::exist(Email email)
+{
+	return false;
+}
+
+bool Email::isValid(Email email)
+{
+	return true;
+}
+
+Email& Email::operator=(const char &assignedEmail)
+{
+	if (!exist && isValid)
+	{
+		email = assignedEmail;
+		return *this;
+	}
+
+
+	// TODO: hier return-Anweisung eingeben
+}
--- SimpleAccountManager/Email.h
+++ SimpleAccountManager/Email.h
@@ -1,5 +1,16 @@
 #pragma once
 class Email
 {
+private:
+	char email;
+
+public:
+	Email(char emailChar);
+	~Email();
+
+	static bool exist(Email email);
+	static bool isValid(Email email);
+
+	Email& operator=(const char& assignedEmail);
 };
 
