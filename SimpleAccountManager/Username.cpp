e59547210b2d75c2b7cf1272734be753bac8b01e
FiluBlack <38252422+FiluBlack@users.noreply.github.com>
2020-08-23 14:39:58 +0200

    Updated Username class

--- SimpleAccountManager/Username.cpp
+++ SimpleAccountManager/Username.cpp
@@ -1,1 +1,29 @@
 #include "Username.h"
+
+Username::Username(char usernameChar)
+{
+	username = usernameChar;
+}
+
+Username::~Username()
+{
+}
+
+bool Username::exist(char username)
+{
+	return false;
+}
+
+bool Username::isValid(char username)
+{
+	return true;
+}
+
+Username& Username::operator=(char &assignedUsername)
+{
+	if (!exist(assignedUsername) && isValid(assignedUsername))
+	{
+		username = assignedUsername;
+		return *this;
+	}
+}
--- SimpleAccountManager/Username.h
+++ SimpleAccountManager/Username.h
@@ -1,5 +1,16 @@
 #pragma once
 class Username
 {
+private:
+	char username;
+
+public:
+	Username(char usernameChar);
+	~Username();
+
+	static bool exist(char username);
+	static bool isValid(char username);
+
+	Username& operator=(char& assignedUsername);
 };
 
